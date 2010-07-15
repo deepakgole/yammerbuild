43588cb1bb56ac8788d70d88a62837b3457cd2bc
Deepak <deepak@sapnasolutions.com>
2010-07-15 17:01:00 +0530

    Updated the files

--- pkg/yammerbuild-0.0.1/Rakefile
+++ pkg/yammerbuild-0.0.1/Rakefile
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'rake'
 require 'echoe'
+require 'lib/yammer_build'
 
 Echoe.new('yammerbuild', '0.0.1') do |p|
   p.description    = "A gem which yammers builds on a network"
@@ -9,4 +10,13 @@ Echoe.new('yammerbuild', '0.0.1') do |p|
   p.email          = "gole.deepak@gmail.com"
   p.ignore_pattern = ["tmp/*", "script/*"]
   p.development_dependencies = []
+end
+
+namespace :yammer do
+
+desc 'This task send a email to yammer'
+  task :send_email do
+    YammerBuild::Notifier.send_email
+  end
+
 end
--- pkg/yammerbuild-0.0.1/lib/yammer_build.rb
+++ pkg/yammerbuild-0.0.1/lib/yammer_build.rb
@@ -10,20 +10,37 @@ module YammerBuild
  class Notifier < ActionMailer::Base
 
   def self.send_email()
-    project_name, success, commit = pull_last_commit_data_from_integrity
-    YammerBuild::Notifier.deliver_email(project_name, success, commit)
+    ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => "/var/www/apps/integrity/integrity.db")
+    build_yml = YAML.load_file("/var/www/apps/yammerbuild/lib/last_build.yml")
+    p build_yml
+    last_build_id = build_yml["last_build_id"]
+    p last_build_id
+    number_of_builds_to_yammer = ActiveRecord::Base.connection.execute("SELECT * FROM 'integrity_builds' where id>#{last_build_id} and completed_at is not NULL")
+    number_of_builds_to_yammer.each do |build|
+#     if build["successful"] != 't'
+      project_name, success, commit = pull_last_commit_data_from_integrity(build["id"])
+      YammerBuild::Notifier.deliver_email(project_name, success, commit)
+#     end
+    end
+    p number_of_builds_to_yammer.last["id"]
+    dump_hash = {"last_build_id"=>number_of_builds_to_yammer.last["id"]}
+    File.open("/var/www/apps/yammerbuild/lib/last_build.yml", 'w') { |f| YAML.dump(dump_hash, f) }
+
   end
 
-  def self.pull_last_commit_data_from_integrity
+  def self.pull_last_commit_data_from_integrity(build_id)
     ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => "/var/www/apps/integrity/integrity.db")
-    build =  ActiveRecord::Base.connection.execute("SELECT MAX ( id ) build_max_id FROM 'integrity_builds'")
-    build = ActiveRecord::Base.connection.execute("SELECT * FROM 'integrity_builds' where id='#{build[0]["build_max_id"]}'")
+#   build =  ActiveRecord::Base.connection.execute("SELECT MAX ( id ) build_max_id FROM 'integrity_builds'")
+
+    build = ActiveRecord::Base.connection.execute("SELECT * FROM 'integrity_builds' where id='#{build_id}'")
     p "=================Build Id=#{build[0]['id']}========"
     commit = ActiveRecord::Base.connection.execute("SELECT * from 'integrity_commits' where id='#{build[0]["commit_id"]}' ")
-    p "=================Commit Id=#{commit[0]['id']}========"
+   p "=================Commit Id=#{commit[0]['id']}========"
     project = ActiveRecord::Base.connection.execute("SELECT * from 'integrity_projects' where id='#{commit[0]["project_id"]}' ")
-    p "=================Prprooject Id=#{project[0]['id']}========"
+    p "=================Project Id=#{project[0]['id']}========"
     success = build[0]["successful"] == 'f' ? "fail" : "pass"
+    p build[0]["successful"]
+    p success
     email_setup(project[0]["name"], success, commit[0]["author"] )
     return project[0]["name"], success, commit[0]
   end
@@ -33,30 +50,36 @@ module YammerBuild
    YammerBuild::Notifier.template_root = File.dirname(__FILE__)
    YammerBuild::Notifier.delivery_method = :smtp
    if success == "pass"
-     YammerBuild::Notifier.smtp_settings = {:address => "smtp.gmail.com",:port => 587,:domain =>  "sapnasolutions.com",
+     YammerBuild::Notifier.smtp_settings = {:enable_starttls_auto => true, :address => "smtp.gmail.com",:port => 587,:domain =>  "sapnasolutions.com",
                              :user_name =>"qa@sapnasolutions.com",:password => "deepak",:authentication => :plain}
    else
-     YammerBuild::Notifier.smtp_settings = {:address => "smtp.gmail.com",:port => 587,:domain =>  "sapnasolutions.com",
+     YammerBuild::Notifier.smtp_settings = {:enable_starttls_auto => true, :address => "smtp.gmail.com",:port => 587,:domain =>  "sapnasolutions.com",
                              :user_name =>"contact@sapnasolutions.com",:password => "xVU8QkcFpdM1",:authentication => :plain}
    end
+   YammerBuild::Notifier.perform_deliveries = true
+   YammerBuild::Notifier.raise_delivery_errors = false
+   YammerBuild::Notifier.default_charset = "utf-8"
+   YammerBuild::Notifier.default_content_type = "text/html"
   end
 
 
   def email(project_name, success, commit)
-    recipients "deepak+sapnasolutions.com@yammer.com"
-    # from "sender@anotherdomain.com"
-    subject "#{project_name} build #{success}"
-    content_type "multipart/alternative"
-    body = { :project_name => project_name, :commit_message => commit["message"], :who_committed => commit["author"]}
-    @project = project_name
-    @message = commit["message"]
-    p "=====================#{@project} #{success}======="
-    @who_committed = commit["author"]
-    @committed_at = commit["committed_at"]
-    part :content_type => "text/plain", :body => render_message('email', body)
+    yammer_groups_yml = YAML.load_file("/var/www/apps/yammerbuild/lib/yammer_groups.yml")
+    yammer_names_yml = YAML.load_file("/var/www/apps/yammerbuild/lib/yammer_names.yml")
+    p "=============groups name=#{yammer_groups_yml[project_name]}========="
+    p commit["author"]
+    p "=============author name=#{yammer_names_yml[commit["author"].split(" <").first]}========="
+    @subject = "#{project_name} build #{success}"
+    @recipients = ["#{yammer_groups_yml[project_name]}", "deepak@sapnasolutions.com"]
+#    @recipients = ["deepak@sapnasolutions.com"]
+    @body["project_name"] = project_name
+    @body["who_committed"] = yammer_names_yml[commit["author"].split(" <").first]
+    @body["message"] = commit["message"]
+    @body["committed_at"] = commit["committed_at"]
   end
 
  end
 
 
 end
+
